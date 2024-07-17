4fdfa90670d1745efb70a904762a5fc465a4dbd5
Akkariin Meiko <akkariins@gmail.com>
2024-07-18 03:03:53 +0800

    Update freemobile.php

--- includes/freemobile.php
+++ includes/freemobile.php
@@ -4,15 +4,16 @@ class FreeMobile {
     public string $user;
     public string $pass;
 
-    public function __construct($user, $pass) {
+    public function __construct($user, $pass)
+    {
         $this->user = $user;
         $this->pass = $pass;
     }
 
-    public function sendMessage($message) {
-        //encode message to %20
-        $message = urlencode($message);
-        $curl = curl_init('https://smsapi.free-mobile.fr/sendmsg?user=' . $this->user . '&pass=' . $this->pass . '&msg=' . $message);
+    public function sendMessage($message)
+    {
+        $curl = curl_init(sprintf('https://smsapi.free-mobile.fr/sendmsg?user=%s&pass=%s&msg=%s',
+            urlencode($this->user), urlencode($this->pass), urlencode($message)));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HEADER, false);
 		return curl_exec($curl);
